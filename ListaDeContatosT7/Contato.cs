84d96071c9590a1dff8b7db99b33f643ec469a52
Laulis <167126200+Laulis@users.noreply.github.com>
2024-04-22 15:21:01 -0300

    Criada a classe contato

---
+++ ListaDeContatosT7/Contato.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatosT7
+{
+    internal class Contato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+
+        public string Nome 
+        {  
+            get 
+            { 
+                return nome; 
+            } 
+            set 
+            { 
+                nome = value; 
+            }
+        }
+        public string Sobrenome
+        {
+            get 
+            {
+            return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        }
+        public string Telefone
+        {
+            get 
+            {
+            return telefone;
+            }
+            set
+            {  
+                if (value.Length == 11)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(00)-000000-0000";
+                }
+            }
+        }
+
+
+        public Contato()
+        {
+            Nome = "José";
+            Sobrenome = "Da Costa";
+            Telefone = "(11)-94002-8922";
+        }
+
+
+        public Contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+
+        public override string ToString()
+        {
+           string saida = string.Empty;
+            saida += String.Format("{0}, {1}", Nome, Sobrenome);
+            saida += String.Format("({0}) {1}-{2}", Telefone.Substring(0, 2), Telefone.Substring(2,5), Telefone.Substring(7,4));
+            return saida;
+        }
+    }
+}
